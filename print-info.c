56b2e0bc5aeaecf3f435d3641548c586b076e92f
Yuchen Jing <y5jing@ucsd.edu>
2024-02-10 19:40:50 -0800

    Fix: threshold inconsistency

--- print-info.c
+++ print-info.c
@@ -6,8 +6,8 @@
 #define TOTAL_ENTRIES_MSG "Total entries: %lu\n"
 #define LONGEST_MSG "Longest chain: %lu\n"
 #define SHORTEST_MSG "Shortest chain: %lu\n"
+#define THRESHOLD_MSG "Reached threshold: %lu\n"
 #define EMPTY_MSG "Empty buckets: %lu\n"
-#define THRESHOLD_MSG "Greater than threshold: %lu\n"
 
 void print_info(Node **htable, size_t table_size, unsigned long threshold) {
 	// TODO: walk down the indices of the htable
